9467a8ef177b5737b60c2457fb7142e6747533ad
pedro-cal <pedroluna2@hotmail.com>
2022-09-05 22:45:05 -0300

    Fixed range selector updates

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 let userData;
+const contentDiv = document.querySelector(".content");
+const headerTag = document.querySelector("#header");
+const rangeValueP = document.querySelector("#range-value");
+
 fetch("https://dummyjson.com/users")
-  .then((response) => response.json())
-  .then((data) => {
-    userData = data.users;
-    console.log(userData);
-  });
+   .then((response) => response.json())
+   .then((data) => {
+      userData = data.users;
+      // console.log(userData);
+   });
+const rangeSelector = document.querySelector("#range");
 const range = document.querySelector("#range").value;
-console.log(userData);
-function rangeSelected() {
-  userData.slice(range).forEach((userData) => {
-    console.log(userData);
-    const div = document.createElement("div");
-    document.querySelector(".content").appendChild(div);
-    div.classList.add("card-box");
-    div.innerHTML = ` <div class ="img">
+// console.log(userData);
+rangeSelector.addEventListener('change', (event) => rangeSelected(event));
+
+// function handleRangeChange(event) {
+//    console.log(event.target.value);
+// }
+
+function rangeSelected(event) {
+   rangeValueP.innerText = event.target.value;
+   contentDiv.innerHTML = '';
+   userData.slice(userData.length - event.target.value).forEach((userData) => {
+      // console.log(userData);
+      const div = document.createElement("div");
+      document.querySelector(".content").appendChild(div);
+      div.classList.add("card-box");
+      div.innerHTML = ` <div class ="img">
     <img src="${userData.image}" alt="" />
     </div>
     <div>
@@ -23,9 +36,9 @@ function rangeSelected() {
     </div>
     <div class="decoration-box"></div>
     `;
-  });
+   });
 }
 
 for (let index = 0; index < 10; index++) {
-  console.log(userData);
+   console.log(userData);
 }
